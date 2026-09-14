412986d4b367fa6bea98dac4b2a62b2f0dbad98a
NancyRashid123 <dyaarashid@icloud.com>
2026-09-14 19:54:15 +0300

    Use current origin for API

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -3,9 +3,8 @@
 import { treaty } from "@elysia/eden";
 import type { app } from "../app/api/[[...slugs]]/route";
 
-const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://realtime-chat-git-main-nancyrashid123s-projects.vercel.app"
-    : "http://localhost:3000";
-
-export const api = treaty<typeof app>(baseUrl).api;
+export const api = treaty<typeof app>(
+  typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000"
+).api;
