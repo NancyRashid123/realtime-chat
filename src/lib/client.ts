d1dbc27f53bb3eee51443acc2c78c1ae519be00e
NancyRashid123 <dyaarashid@icloud.com>
2026-09-14 19:34:15 +0300

    Fix API client for deployment

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,10 @@
+"use client";
+
 import { treaty } from "@elysia/eden";
 import type { app } from "../app/api/[[...slugs]]/route";
 
-export const api = treaty<typeof app>("http://localhost:3000").api;
+export const api = treaty<typeof app>(
+  typeof window !== "undefined"
+    ? window.location.origin
+    : "http://localhost:3000"
+).api;
