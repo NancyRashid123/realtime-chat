0f3843ed880fd56caac57e36a2a9fb090779e47d
NancyRashid123 <dyaarashid@icloud.com>
2026-09-14 19:52:12 +0300

    Fix production API URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,7 +5,7 @@ import type { app } from "../app/api/[[...slugs]]/route";
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://realtime-chat-git-main-nancyrashid123s-projects.vercel.app/api"
+    ? "https://realtime-chat-git-main-nancyrashid123s-projects.vercel.app"
     : "http://localhost:3000";
 
 export const api = treaty<typeof app>(baseUrl).api;
